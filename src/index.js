0733a9f6f3f76b7be4a4f27dd715dadba2b88462
stoffern <kristoffer.klevenberg@gmail.com>
2017-11-01 18:19:04 +0100

    fix(dep): Fix dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom'
-import AppContainer from 'react-hot-loader/lib/AppContainer'
 import winston from 'winston'
 
 import Config from './classes/Config';
