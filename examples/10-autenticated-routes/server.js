a23f98daf19d77acd7f66416c814d346729b8e8b
stoffern <kristoffer.klevenberg@gmail.com>
2018-02-23 08:54:36 +0100

    fix(Travis): Semantic Release test

--- examples/10-autenticated-routes/server.js
+++ examples/10-autenticated-routes/server.js
@@ -20,18 +20,18 @@ server.addPassportStrategy(
 /**
  * ADD ROUTES
  */
-// const route = server.addReactRoute(
-//   "", //Route prefix
-//   path.resolve(process.cwd(), "app/Routes.js"), //Path to react app, must return a found router..
-//   {
-//     entry: [
-//       "webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=false&quiet=false&noInfo=false",
-//       "react-hot-loader/patch",
-//       path.resolve(process.cwd(), "../../src/utils/client-render")
-//     ]
-//   }, // YOU DONT NEED THIS (ONLY IN USE FOR THIS EXAMPLE TO RUN)
-//   { entry: path.resolve(process.cwd(), "../../src/utils/server-render") } // YOU DONT NEED THIS (ONLY IN USE FOR THIS EXAMPLE TO RUN)
-// );
+const route = server.addReactRoute(
+  "", //Route prefix
+  path.resolve(process.cwd(), "app/Routes.js"), //Path to react app, must return a found router..
+  {
+    entry: [
+      "webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=false&quiet=false&noInfo=false",
+      "react-hot-loader/patch",
+      path.resolve(process.cwd(), "../../src/utils/client-render")
+    ]
+  }, // YOU DONT NEED THIS (ONLY IN USE FOR THIS EXAMPLE TO RUN)
+  { entry: path.resolve(process.cwd(), "../../src/utils/server-render") } // YOU DONT NEED THIS (ONLY IN USE FOR THIS EXAMPLE TO RUN)
+);
 
 var staticRoute = server.addStaticFile(
   path.resolve(process.cwd(), "static", "hello.txt")
@@ -44,17 +44,17 @@ var staticRoute2 = server.addStaticFolder(
 /**
  * ADD AUTHENTICATION
  */
-// route.addAuthentication((passport, ctx, next) => {
-//   passport.authenticate("basic", (err, user, info) => {
-//     if (user) ctx.login(user);
-//     else ctx.logout();
-//   })(ctx, next);
+route.addAuthentication((passport, ctx, next) => {
+  passport.authenticate("basic", (err, user, info) => {
+    if (user) ctx.login(user);
+    else ctx.logout();
+  })(ctx, next);
 
-//   if (!ctx.isAuthenticated()) {
-//     ctx.status = 404;
-//     ctx.body = "USER NOT FOUND";
-//   }
-// });
+  if (!ctx.isAuthenticated()) {
+    ctx.status = 404;
+    ctx.body = "USER NOT FOUND";
+  }
+});
 
 staticRoute2.addAuthentication((passport, ctx, next) => {
   return passport.authenticate("basic", { session: false })(ctx, next);
